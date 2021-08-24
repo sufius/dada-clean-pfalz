493ee547566434d0980cc8ba3a6186e5501e9037
imvlandau <itimvlandau@gmail.com>
2021-08-24 20:38:53 +0200

    cleanup add stepper

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import {
   createTheme,
   responsiveFontSizes
 } from "@material-ui/core/styles";
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 import Link from "@material-ui/core/Link";
-import ProTip from "./ProTip";
-import DcpAppBar from "./components/DcpAppBar";
+import Container from "@material-ui/core/Container";
 import grey from "@material-ui/core/colors/grey";
+import DcpAppBar from "./components/DcpAppBar";
+import DcpStepper from "./components/DcpStepper";
 
 const theme = responsiveFontSizes(
   createTheme({
@@ -48,7 +48,7 @@ const theme = responsiveFontSizes(
           }
         }
       }
-    },
+    }
   })
 );
 
@@ -66,18 +66,52 @@ function Copyright() {
 
 export default function App() {
   return (
-  <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <DcpAppBar />
-      <Container maxWidth="sm">
-        <Box sx={{ my: 4 }}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Create React App v5-beta example with TypeScript
-          </Typography>
-          <ProTip />
-          <Copyright />
-        </Box>
-      </Container>
+      <DcpAppBar>
+        <Container maxWidth="lg">
+          <Box sx={{ mt: 1, mb: 4 }}>
+            <Typography variant="h4" component="h1" gutterBottom>
+              Create React App v5-beta example with TypeScript
+            </Typography>
+
+            <Typography paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Rhoncus dolor purus non enim praesent elementum facilisis leo vel.
+              Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+              gravida rutrum quisque non tellus. Convallis convallis tellus id
+              interdum velit laoreet id donec ultrices. Odio morbi quis commodo
+              odio aenean sed adipiscing. Amet nisl suscipit adipiscing bibendum
+              est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+              Metus vulputate eu scelerisque felis imperdiet proin fermentum
+              leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt
+              lobortis feugiat vivamus at augue. At augue eget arcu dictum
+              varius duis at consectetur lorem. Velit sed ullamcorper morbi
+              tincidunt. Lorem donec massa sapien faucibus et molestie ac.
+            </Typography>
+            <Typography paragraph>
+              Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+              ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+              elementum integer enim neque volutpat ac tincidunt. Ornare
+              suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
+              volutpat consequat mauris. Elementum eu facilisis sed odio morbi.
+              Euismod lacinia at quis risus sed vulputate odio. Morbi tincidunt
+              ornare massa eget egestas purus viverra accumsan in. In hendrerit
+              gravida rutrum quisque non tellus orci ac. Pellentesque nec nam
+              aliquam sem et tortor. Habitant morbi tristique senectus et.
+              Adipiscing elit duis tristique sollicitudin nibh sit. Ornare
+              aenean euismod elementum nisi quis eleifend. Commodo viverra
+              maecenas accumsan lacus vel facilisis. Nulla posuere sollicitudin
+              aliquam ultrices sagittis orci a.
+            </Typography>
+          </Box>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <DcpStepper />
+          </Box>
+        </Container>
+      </DcpAppBar>
+      <Copyright />
     </ThemeProvider>
   );
 }
