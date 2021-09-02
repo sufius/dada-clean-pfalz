f2ebd3e6dd872fe1d2fd8b66a3202b73c31e646f
imvlandau <itimvlandau@gmail.com>
2021-09-02 16:19:33 +0200

    deactivat loop in carousel (not working besides parallax bg) replace background image adjust css

--- src/components/DcpCarousel/DcpCarousel.tsx
+++ src/components/DcpCarousel/DcpCarousel.tsx
@@ -20,15 +20,15 @@ const DcpCarousel = () => {
           "--swiper-pagination-color": "#fff"
         }}
         speed={600}
-        loop={true}
+        loop={false}
         parallax={true}
         autoplay={{
-         "delay": 6000,
-         "disableOnInteraction": false
+          delay: 6000,
+          disableOnInteraction: false
         }}
         pagination={{
           clickable: true,
-          renderBullet: function (index, className) {
+          renderBullet: function(index, className) {
             return '<span class="' + className + '">' + (index + 1) + "</span>";
           }
         }}
@@ -39,48 +39,65 @@ const DcpCarousel = () => {
           slot="container-start"
           className="parallax-bg"
           style={{
-            backgroundImage:
-              "url(https://swiperjs.com/demos/images/nature-1.jpg)"
+            backgroundImage: "url(/background_testimonials.jpg)"
           }}
           data-swiper-parallax="-23%"
         ></div>
         <SwiperSlide>
-          <div className="title" data-swiper-parallax="-300">
-            GRUNDREINIGUNG
-          </div>
-          <div className="subtitle" data-swiper-parallax="-200">
-            Maria S.
-          </div>
-          <div className="text" data-swiper-parallax="-100">
-            <p>
-            Ich war mit der Qualität der geleisteten Arbeit sehr zufrieden! Die Arbeiten wurden auf höchstem Niveau und innerhalb der vereinbarten Zeit erledigt. Ich möchte euch meinen herzlichsten Dank aussprechen, nicht nur für die hervorragende Arbeit, sondern auch für die angenehme Arbeitsatmosphäre!
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="title" data-swiper-parallax="-300">
-            WOHNHAUSREINIGUNG
-          </div>
-          <div className="subtitle" data-swiper-parallax="-200">
-            Niklas C.
-          </div>
-          <div className="text" data-swiper-parallax="-100">
-            <p>
-              Ich habe eine Reinigung der Wohnung und das putzen der Fenster bestellt. Sie haben alles mit hoher Qualität erledigt. Alles glänzt blitzeblank. Ich bin sehr zufrieden und werde diesen Service auch in Zukunft in Anspruch nehmen. Nochmals vielen Dank euch.
-            </p>
+          <div className="content">
+            <div className="title" data-swiper-parallax="-300">
+              GRUNDREINIGUNG
+            </div>
+            <div className="subtitle" data-swiper-parallax="-200">
+              Maria S.
+            </div>
+            <div className="text" data-swiper-parallax="-100">
+              <p>
+                Ich war mit der Qualität der geleisteten Arbeit sehr zufrieden!
+                Die Arbeiten wurden auf höchstem Niveau und innerhalb der
+                vereinbarten Zeit erledigt. Ich möchte euch meinen herzlichsten
+                Dank aussprechen, nicht nur für die hervorragende Arbeit,
+                sondern auch für die angenehme Arbeitsatmosphäre!
+              </p>
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="title" data-swiper-parallax="-300">
-            GEBÄUDEREINIGUNG
+          <div className="content">
+            <div className="title" data-swiper-parallax="-300">
+              WOHNHAUSREINIGUNG
+            </div>
+            <div className="subtitle" data-swiper-parallax="-200">
+              Niklas C.
+            </div>
+            <div className="text" data-swiper-parallax="-100">
+              <p>
+                Ich habe eine Reinigung der Wohnung und das putzen der Fenster
+                bestellt. Sie haben alles mit hoher Qualität erledigt. Alles
+                glänzt blitzeblank. Ich bin sehr zufrieden und werde diesen
+                Service auch in Zukunft in Anspruch nehmen. Nochmals vielen Dank
+                euch.
+              </p>
+            </div>
           </div>
-          <div className="subtitle" data-swiper-parallax="-200">
-            Paul G.
-          </div>
-          <div className="text" data-swiper-parallax="-100">
-            <p>
-              Ich bestelle zum dritten Mal eine Generalreinigung der Wohnung. Die erbrachte Leistung war einwandfrei und der Preis ist günstiger als bei den meisten anderen Reinigungsfirmen, die ich kenne. Ich empfehle diese Firma jedem, der eine qualitativ hochwertige und schnelle Reinigung benötigt!!!
-            </p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="content">
+            <div className="title" data-swiper-parallax="-300">
+              GEBÄUDEREINIGUNG
+            </div>
+            <div className="subtitle" data-swiper-parallax="-200">
+              Paul G.
+            </div>
+            <div className="text" data-swiper-parallax="-100">
+              <p>
+                Ich bestelle zum dritten Mal eine Generalreinigung der Wohnung.
+                Die erbrachte Leistung war einwandfrei und der Preis ist
+                günstiger als bei den meisten anderen Reinigungsfirmen, die ich
+                kenne. Ich empfehle diese Firma jedem, der eine qualitativ
+                hochwertige und schnelle Reinigung benötigt!!!
+              </p>
+            </div>
           </div>
         </SwiperSlide>
       </Swiper>
