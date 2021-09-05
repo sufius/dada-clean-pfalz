f3a202134fd79d5645e357cbd90532d32739f617
imvlandau <itimvlandau@gmail.com>
2021-09-05 04:37:32 +0200

    cleanup

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -40,7 +40,7 @@ const Hero = () => {
       >
         <Box
           sx={{
-            display: 'flex',
+            display: "flex",
             flexDirection: "column",
             justifyContent: "center",
             alignItems: "center",
