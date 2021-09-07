abde4c0be18a79e8ebdac3d7a8a4ccf504ffc91f
imvlandau <itimvlandau@gmail.com>
2021-09-07 17:50:42 +0200

    extract sendMail function fix wrong placement of comments in .env files

--- scripts/server.js
+++ scripts/server.js
@@ -1,64 +1,18 @@
-"use strict";
 const express = require("express");
 const proxy = require("http-proxy-middleware");
 const http = require("http");
 const fs = require("fs");
-const nodemailer = require("nodemailer");
 var bodyParser = require("body-parser");
+const path = require("path");
 const { body } = require("express-validator");
+const sendMail = require("./sendMail");
 
 require("dotenv").config({ path: ".env.local" });
 
-// async..await is not allowed in global scope, must use a wrapper
-async function main(name, email, mobile, emailText) {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  // let testAccount = await nodemailer.createTestAccount();
-
-  // create reusable transporter object using the default SMTP transport
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: process.env.SMTP_SECURE, // use SSL
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS
-    }
-  });
-
-  let message = `
-TEL.:
-${mobile}
-
-E-MAIL:
-${email}
-
-NAME:
-${name}
-
-
-${emailText}
-`;
-
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: `"${name}" <${email}>`, // sender address
-    replyTo: email,
-    to: process.env.SMTP_USER, // list of receivers
-    subject: `Nachricht von ${name}`,
-    text: message // plain text body
-  });
-
-  // console.log("Message sent: %s", info.messageId);
-  // // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-  //
-  // // Preview only available when sending through an Ethereal account
-  // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-}
-
 const app = express();
 
+// Serve any static files
+app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(
   bodyParser.urlencoded({
@@ -66,56 +20,61 @@ app.use(
     extended: true
   })
 );
-app.post(
-  "/request/quotation",
-  body("name")
-    .not()
-    .isEmpty()
-    .trim()
-    .escape(),
-  body("email")
-    .not()
-    .isEmpty()
-    .trim()
-    .isEmail()
-    .normalizeEmail({ gmail_remove_dots: false }),
-  body("emailText")
-    .not()
-    .isEmpty()
-    .trim()
-    .escape(),
+
+app.post("/request/quotation",
+  body("name").not().isEmpty().trim().escape(),
+  body("email").not().isEmpty().trim().isEmail().normalizeEmail({ gmail_remove_dots: false }),
+  body("mobile").not().isEmpty().trim().escape(),
+  body("emailText").not().isEmpty().trim().escape(),
   (req, res) => {
-    // console.log("[PMB] Requested URL:", req.url);
+    if (
+      process.env.NODE_ENV === "development" ||
+      process.env.NODE_ENV === "dev"
+    ) {
+      console.log("[PMB] Requested URL:", req.url);
+    }
     let name = req.body.name;
     let email = req.body.email;
     let mobile = req.body.mobile;
     let emailText = req.body.emailText;
     if (!name || !email || !mobile || !emailText) {
       res.sendStatus(500);
-    } else {
-      main(name, email, mobile, emailText)
-        .then(() => {
-          res.sendStatus(200);
-        })
-        .catch(error => {
-          console.error(error);
-          res.sendStatus(500);
-        });
-    }
+  } else {
+    sendMail(name, email, mobile, emailText)
+      .then(() => {
+        res.sendStatus(200);
+      })
+      .catch(err => {
+        if (
+          process.env.NODE_ENV === "development" ||
+          process.env.NODE_ENV === "dev"
+        ) {
+          console.error(err);
+        }
+        res.status(500).send(err);
+      });
   }
-);
-
-// Serve any static files
-app.use(express.static("/var/www/dada-clean-pfalz/build"));
+});
 
 // Handle React routing, return all requests to landing page
 app.get("/*", function(req, res) {
-  console.log(
-    "[IMV] Requested URL for /*:",
-    new Date().toLocaleString(),
-    req.url
-  );
-  res.sendFile(path.join("/var/www/dada-clean-pfalz/build", "index.html"));
+  if (
+    process.env.NODE_ENV === "development" ||
+    process.env.NODE_ENV === "dev"
+  ) {
+    console.log(
+      "[IMV] Requested URL for /*:",
+      new Date().toLocaleString(),
+      req.url
+    );
+  }
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"), function(
+    err
+  ) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
 });
 
 http.createServer(app).listen(process.env.PORT || 80);
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,56 +1,11 @@
 "use strict";
 const nodemailer = require("nodemailer");
 var bodyParser = require("body-parser");
+const path = require("path");
 const { body } = require("express-validator");
+const sendMail = require(path.join(__dirname, "..", "scripts", "sendMail"));
+
 require("dotenv").config({ path: '.env.local' });
-
-// async..await is not allowed in global scope, must use a wrapper
-async function main(name, email, mobile, emailText) {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  // let testAccount = await nodemailer.createTestAccount();
-
-  // create reusable transporter object using the default SMTP transport
-  const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
-      secure: process.env.SMTP_SECURE, // use SSL
-      auth: {
-          user: process.env.SMTP_USER,
-          pass: process.env.SMTP_PASS
-      }
-  });
-
-  let message = `
-TEL.:
-${mobile}
-
-E-MAIL:
-${email}
-
-NAME:
-${name}
-
-
-${emailText}
-`;
-
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: `"${name}" <${email}>`, // sender address
-    replyTo: email,
-    to: process.env.SMTP_USER, // list of receivers
-    subject: `Nachricht von ${name}`,
-    text: message // plain text body
-  });
-
-  // console.log("Message sent: %s", info.messageId);
-  // // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-  //
-  // // Preview only available when sending through an Ethereal account
-  // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-}
 
 // this is used just in mode: NODE_ENV === "development"
 module.exports = function(app) {
@@ -65,9 +20,15 @@ module.exports = function(app) {
     "/request/quotation",
     body("name").not().isEmpty().trim().escape(),
     body("email").not().isEmpty().trim().isEmail().normalizeEmail({ gmail_remove_dots: false}),
+    body("mobile").not().isEmpty().trim().escape(),
     body("emailText").not().isEmpty().trim().escape(),
     (req, res) => {
-      // console.log("[PMB] Requested URL:", req.url);
+      if (
+        process.env.NODE_ENV === "development" ||
+        process.env.NODE_ENV === "dev"
+      ) {
+        console.log("[PMB] Requested URL:", req.url);
+      }
       let name = req.body.name;
       let email = req.body.email;
       let mobile = req.body.mobile;
@@ -75,11 +36,16 @@ module.exports = function(app) {
       if (!name || !email || !mobile || !emailText) {
           res.sendStatus(500);
       } else {
-        main(name, email, mobile, emailText).then(() => {
+        sendMail(name, email, mobile, emailText).then(() => {
           res.sendStatus(200);
         }).catch(error => {
-          console.error(error);
-          res.sendStatus(500);
+          if (
+            process.env.NODE_ENV === "development" ||
+            process.env.NODE_ENV === "dev"
+          ) {
+            console.error(err);
+          }
+          res.status(500).send(err);
         });
       }
     }
