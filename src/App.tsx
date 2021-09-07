3e19307aa031d60343f6f888c23eccc5cb63e30c
imvlandau <itimvlandau@gmail.com>
2021-09-07 02:30:43 +0200

    add new privacy policy page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import * as React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import LegalNotes from "./pages/LegalNotes";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
 
 export default function App() {
   return (
     <Router>
       <Route exact path="/impressum">
         <LegalNotes />
+      </Route>
+      <Route exact path="/datenschutz">
+        <PrivacyPolicy />
       </Route>
       <Route exact path={"/"}>
         <Home />
