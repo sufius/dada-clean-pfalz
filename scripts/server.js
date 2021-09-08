2063ea4f38caf6e8b706c394df1fc3cad8e1ec10
imvlandau <itimvlandau@gmail.com>
2021-09-08 02:39:28 +0200

    add https support to server.js

--- scripts/server.js
+++ scripts/server.js
@@ -1,15 +1,39 @@
 const express = require("express");
 const proxy = require("http-proxy-middleware");
-const http = require("http");
-const fs = require("fs");
-var bodyParser = require("body-parser");
 const path = require("path");
+const http = require("http");
+const https = require("https");
+const fs = require("fs");
+const bodyParser = require("body-parser");
 const { body } = require("express-validator");
 const sendMail = require("./sendMail");
 
 require("dotenv").config({ path: ".env.local" });
 
 const app = express();
+
+const sslOptions = process.env.HTTPS === "true" && {
+  cert: fs.readFileSync(
+    process.env.SSL_CRT_FILE,
+    "utf8"
+  ),
+  key: fs.readFileSync(
+    process.env.SSL_KEY_FILE,
+    "utf8"
+  )
+};
+
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure &&
+      req.get('x-forwarded-proto') !== 'https' &&
+      process.env.NODE_ENV !== "development" &&
+      process.env.NODE_ENV !== "dev") {
+        return res.redirect('https://' + req.headers.host + req.url);
+  }
+  next();
+}
+app.use(requireHTTPS);
 
 // Serve any static files
 app.use(express.static(path.join(__dirname, "..", "build"), { dotfiles: 'allow' }));
@@ -78,3 +102,5 @@ app.get("/*", function(req, res) {
 });
 
 http.createServer(app).listen(process.env.PORT || 80);
+process.env.HTTPS === "true" &&
+  https.createServer(sslOptions, app).listen(process.env.PORT_SSL || 443);
