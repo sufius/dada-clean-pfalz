620c532785e11d935a2ca3dcaf2fcca71284ebf6
imvlandau <itimvlandau@gmail.com>
2022-09-10 19:11:38 +0200

    remove nodemailer

--- scripts/server.js
+++ scripts/server.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const proxy = require("http-proxy-middleware");
 const path = require("path");
 const http = require("http");
 const https = require("https");
 const fs = require("fs");
 const bodyParser = require("body-parser");
 const { body } = require("express-validator");
-const sendMail = require("./sendMail");
 
 require("dotenv").config({ path: ".env.local" });
 
@@ -48,41 +46,6 @@ app.use(
     extended: true
   })
 );
-
-app.post("/request/quotation",
-  body("name").not().isEmpty().trim().escape(),
-  body("email").not().isEmpty().trim().isEmail().normalizeEmail({ gmail_remove_dots: false }),
-  body("mobile").not().isEmpty().trim().escape(),
-  body("emailText").not().isEmpty().trim().escape(),
-  (req, res) => {
-    if (
-      process.env.NODE_ENV === "development" ||
-      process.env.NODE_ENV === "dev"
-    ) {
-      console.log("[PMB] Requested URL:", req.url);
-    }
-    let name = req.body.name;
-    let email = req.body.email;
-    let mobile = req.body.mobile;
-    let emailText = req.body.emailText;
-    if (!name || !email || !mobile || !emailText) {
-      res.sendStatus(500);
-  } else {
-    sendMail(name, email, mobile, emailText)
-      .then(() => {
-        res.sendStatus(200);
-      })
-      .catch(err => {
-        if (
-          process.env.NODE_ENV === "development" ||
-          process.env.NODE_ENV === "dev"
-        ) {
-          console.error(err);
-        }
-        res.status(500).send(err);
-      });
-  }
-});
 
 // Handle React routing, return all requests to landing page
 app.get("/*", function(req, res) {
